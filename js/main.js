518746164de562447ff8dbe681ceec26a4d51a05
hiram@shopphi.xyz <hiram@shopphi.xyz>
2021-06-07 21:18:21 -0500

    for review 1

--- js/main.js
+++ js/main.js
@@ -121,3 +121,23 @@ setTimeout(function(){
     })
 }, 250);
 })
+
+$("#cuatroNavDrop").click(function(){
+    $(".mainLeft").addClass("animate__animated animate__backOutLeft")
+    $(".mainRight").addClass("animate__animated animate__backOutRight")
+
+setTimeout(function(){
+    $.ajax({
+        type: "GET",
+        url: "4.html",
+        success: function(data){
+            $("#main").html(data)
+            $(".mainLeft").addClass("animate__animated animate__backInLeft")
+            $(".mainRight").addClass("animate__animated animate__backInRight")
+            $(".nav-link").removeClass("active")
+            $("#navbarDropdownMenuLink").addClass("active")
+            $('html, body').animate({ scrollTop: 0 }, 0);
+        }
+    })
+}, 250);
+})
