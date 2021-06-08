1da8df453ed2e21d82b01928450d3f24f52987a8
hiram@shopphi.xyz <hiram@shopphi.xyz>
2021-06-07 20:29:44 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,123 @@
+
+$(document).ready(function(){
+    $.ajax({
+        type: "GET",
+        url: "main.html",
+        success: function(data){
+            $("#main").html(data)
+            $(".mainLeft").addClass("animate__animated animate__backInLeft")
+            $(".mainRight").addClass("animate__animated animate__backInRight")
+            setTimeout(function() {
+
+                $("#iconoInicioImg").show()
+                $(".tituloIntro1").show()
+                $(".tituloIntro2").show()
+                $("#botonesInicio").show()
+
+                $("#iconoInicioImg").addClass("animate__animated animate__backInDown")
+                $(".tituloIntro1").addClass("animate__animated animate__backInLeft")
+                $(".tituloIntro2").addClass("animate__animated animate__backInLeft")
+                $("#botonesInicio").addClass("animate__animated animate__backInUp")
+            }, 1000);
+            
+            $("#inicioLinkNav").addClass("active")
+        }
+    })
+})
+
+
+
+$("#inicioLinkNav").click(function(){
+    $(".mainLeft").addClass("animate__animated animate__backOutLeft")
+    $(".mainRight").addClass("animate__animated animate__backOutRight")
+
+setTimeout(function(){
+    $.ajax({
+        type: "GET",
+        url: "main.html",
+        success: function(data){
+            $("#main").html(data)
+            $(".mainLeft").addClass("animate__animated animate__backInLeft")
+            $(".mainRight").addClass("animate__animated animate__backInRight")
+            $(".nav-link").removeClass("active")
+            $("#inicioLinkNav").addClass("active")
+            $('html, body').animate({ scrollTop: 0 }, 0);
+            setTimeout(function() {
+
+                $("#iconoInicioImg").show()
+                $(".tituloIntro1").show()
+                $(".tituloIntro2").show()
+                $("#botonesInicio").show()
+
+                $("#iconoInicioImg").addClass("animate__animated animate__backInDown")
+                $(".tituloIntro1").addClass("animate__animated animate__backInLeft")
+                $(".tituloIntro2").addClass("animate__animated animate__backInLeft")
+                $("#botonesInicio").addClass("animate__animated animate__backInUp")
+            }, 1000);
+        }
+    })
+}, 250);
+})
+
+
+
+
+$("#unoNavDrop").click(function(){
+    $(".mainLeft").addClass("animate__animated animate__backOutLeft")
+    $(".mainRight").addClass("animate__animated animate__backOutRight")
+
+setTimeout(function(){
+    $.ajax({
+        type: "GET",
+        url: "1.html",
+        success: function(data){
+            $("#main").html(data)
+            $(".mainLeft").addClass("animate__animated animate__backInLeft")
+            $(".mainRight").addClass("animate__animated animate__backInRight")
+            $(".nav-link").removeClass("active")
+            $("#navbarDropdownMenuLink").addClass("active")
+            $('html, body').animate({ scrollTop: 0 }, 0);
+        }
+    })
+}, 250);
+})
+
+$("#dosNavDrop").click(function(){
+    $(".mainLeft").addClass("animate__animated animate__backOutLeft")
+    $(".mainRight").addClass("animate__animated animate__backOutRight")
+
+setTimeout(function(){
+    $.ajax({
+        type: "GET",
+        url: "2.html",
+        success: function(data){
+            $("#main").html(data)
+            $(".mainLeft").addClass("animate__animated animate__backInLeft")
+            $(".mainRight").addClass("animate__animated animate__backInRight")
+            $(".nav-link").removeClass("active")
+            $("#navbarDropdownMenuLink").addClass("active")
+            $('html, body').animate({ scrollTop: 0 }, 0);
+        }
+    })
+}, 250);
+})
+
+$("#tresNavDrop").click(function(){
+    $(".mainLeft").addClass("animate__animated animate__backOutLeft")
+    $(".mainRight").addClass("animate__animated animate__backOutRight")
+
+setTimeout(function(){
+    $.ajax({
+        type: "GET",
+        url: "3.html",
+        success: function(data){
+            $("#main").html(data)
+            $(".mainLeft").addClass("animate__animated animate__backInLeft")
+            $(".mainRight").addClass("animate__animated animate__backInRight")
+            $(".nav-link").removeClass("active")
+            $("#navbarDropdownMenuLink").addClass("active")
+            $('html, body').animate({ scrollTop: 0 }, 0);
+        }
+    })
+}, 250);
+})
